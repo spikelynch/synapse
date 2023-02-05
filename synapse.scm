b532fe6147c3d5e0901640432e77f4a2e74f5a8e
Mike Lynch <mike@mikelynch.org>
2023-02-05 11:02:39 +1100

    Firing increments connected neurons

--- synapse.scm
+++ synapse.scm
@@ -13,12 +13,14 @@
 		((receive input)
 			(define new-value (+ value input))
 			(if (> new-value threshold)
-				(bcom (^neuron bcom name threshold 0 connections)
-					(format #f "[~a] Fired!" name))
+				(begin
+					(map (lambda (n) ($ n 'receive 1)) connections)
+					(bcom (^neuron bcom name threshold 0 connections)
+						(format #f "[~a] Fired!" name)))
 				(bcom (^neuron bcom name threshold new-value connections)
 					(format #f "[~a] increment to ~a" name new-value))))))
 
 
 
 
-;;(map (lambda (n) ($ n 'receive 1)) connections)
+;;
