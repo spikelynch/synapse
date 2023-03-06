776a3b1c72ac4fbeff6894f09e35ada3b2c38e7b
Mike Lynch <mike@mikelynch.org>
2023-03-06 14:27:26 +1100

    Drawing links between nodes

--- synapse.scm
+++ synapse.scm
@@ -1,6 +1,4 @@
 
-;; todo for today - try to get the chickadee-vat loading and "running"
-;; in some sens
 
 
 (use-modules 
@@ -26,22 +24,23 @@
 
 
 
-(define (^neuron bcom name threshold value connections)
+(define (^neuron bcom name threshold value pos connections)
   (methods
     ((get-value) value)
     ((get-name) name)
     ((get-connections) connections)
+    ((get-pos) pos)
     ((connect neuron)
       (begin
         (format #t "[~a] adding connection to [~a]\n" name ($ neuron 'get-name))
-        (bcom (^neuron bcom name threshold value (cons neuron connections)))))
+        (bcom (^neuron bcom name threshold value pos (cons neuron connections)))))
     ((receive input)
       (define new-value (+ value input))
       (if (>= new-value threshold)
         (begin
           (for-each (lambda (n) ($ n 'receive 1)) connections)
-          (bcom (^neuron bcom name threshold 0 connections)))
-        (bcom (^neuron bcom name threshold new-value connections))))))
+          (bcom (^neuron bcom name threshold 0 pos connections)))
+        (bcom (^neuron bcom name threshold new-value pos connections))))))
 
 
 (define my-vat (make-chickadee-vat #:agenda (current-agenda)))
@@ -60,14 +59,17 @@
 (define white (make-color 1.0 1.0 1.0 0.78))
 (define color-off (make-color 1.0 1.0 1.0 0))
 
+(define link-color (rgba #x003040))
+(define link-width 1)
 
 (define* (create-node name pos threshold)
 	(list
-    (cons 'neuron (with-vat my-vat (spawn ^neuron name threshold 0 '())))
+    (cons 'neuron (with-vat my-vat (spawn ^neuron name threshold 0 pos '())))
     (cons 'name name)
 		(cons 'pos pos)
 		(cons 'threshold threshold)
 		(cons 'value 0)
+    (cons 'links '())
     (cons 'color color-off)))
 
 (define (add-node! name pos threshold)
@@ -103,10 +105,34 @@
             (let ((neuron (assoc-ref n 'neuron)))
               (begin
                 (assoc-set! n 'value ($ neuron 'get-value))
-                (assoc-set! n 'color (node-color n))
-                )))
+                (assoc-set! n 'color (node-color n)))))
             nodes))
-       (sleep 0.05))))
+       (sleep 0.02))))
+
+
+(define links-script
+    (script
+     (while #t
+      (with-vat my-vat
+        (for-each (lambda (n)
+          (let ((neuron (assoc-ref n 'neuron)))
+            (assoc-set! n 'links 
+              (map
+                (lambda (c) ($ c 'get-pos))
+                ($ neuron 'get-connections)))))
+          nodes)
+        (sleep 0.2)))))
+
+
+(define (update-links)
+  (for-each (lambda (n)
+    (let ((neuron (assoc-ref n 'neuron)))
+      (assoc-set! n 'links 
+        (map
+          (lambda (c) ($ c 'get-pos))
+            ($ neuron 'get-connections)))))
+  nodes))
+
 
 
 ;; load isn't working for me with chickadee play
@@ -124,7 +150,8 @@
           (let ((last-n (assoc-ref (cadr nodes) 'neuron))
                 (new-n (assoc-ref (car nodes) 'neuron)))
             (with-vat my-vat
-              ($ last-n 'connect new-n))))))))
+              ($ last-n 'connect new-n)
+              (update-links))))))))
 
 
 
@@ -141,12 +168,27 @@
     (fill-and-stroke
       (circle (assoc-ref node 'pos) (+ 10 (* 5 (assoc-ref node 'value)))))))
 
-
 (define (paint-nodes nodes)
   (apply superimpose (map paint-node nodes)))
 
+
+(define (paint-link v1 v2)
+  (with-style ((stroke-color link-color) (stroke-width link-width))
+    (stroke (line v1 v2))))
+
+
+;; returns a list of paths of edges from this node
+(define (paint-node-links node)
+  (let ((links (assoc-ref node 'links))
+        (posn (assoc-ref node 'pos)))
+    (apply superimpose (map (lambda (l) (paint-link posn l)) links))))
+
+(define (paint-links nodes)
+  (apply superimpose (map paint-node-links nodes)))
+
 (define (draw alpha)
-  (draw-canvas (make-canvas (paint-nodes nodes))))
+  (draw-canvas (make-canvas
+    (superimpose (paint-links nodes) (paint-nodes nodes)))))
 
 (define (update dt)
   (let ((dt-seconds (/ dt 1000.0)))
